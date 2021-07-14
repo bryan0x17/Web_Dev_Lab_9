df768963b4aed966b770f47fd25d56cd7a4d5fba
Orobs <50989230+bryan0x17@users.noreply.github.com>
2021-07-13 21:41:05 -0600

    Search by name

--- lab9/lab9.js
+++ lab9/lab9.js
@@ -1,121 +1,126 @@
-/*jsonfilehandler.js*/
-var xhr = new XMLHttpRequest();
-var r;
-window.onload=loaddata;
-function loaddata() {
-	//event listener
-	document.getElementById("fullname").addEventListener("input", function (){ searchFullName(this.value);},false);
-	document.getElementById("idnumber").addEventListener("input", function (){ searchIDNumber(this.value);},false);
-	document.getElementById("company").addEventListener("input", function (){ searchCompanyName(this.value);},false);
-	
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-     r = JSON.parse(xhr.responseText);
-      //displayData(r);
-    }
-  };
-  xhr.open("GET", "employeedata.json", true);
-  xhr.send();
-	
-	
-	
+let dataRequest = new XMLHttpRequest();
+let clientData;
 
+const searchBy = 'Results using: ';
+
+const searchHeadings = '<tr><th>Client ID</th>'
+    + '<th>First Name</th>'
+    + '<th>Last Name</th>'
+    + '<th>Address</th>'
+    + '<th>Postal Code</th>'
+    + '<th>First Name</th>'
+    + '<th>Phone Number</th>'
+    + '<th>Address</th>'
+    + '<th>Type</th></tr>';
+
+function setEventListeners() {
+	document.getElementById('name').addEventListener('input', function() {searchName(this.value);});
+	document.getElementById('id').addEventListener('input', function() {searchID(this.value);});
+	document.getElementById('phone').addEventListener('input', function() {searchPhone(this.value);});
+	
+    dataRequest.onreadystatechange = function() {
+        if (dataRequest.readyState == 4 && dataRequest.status == 200) {
+        clientData = JSON.parse(dataRequest.responseText);
+        }
+    };
+    dataRequest.open('GET', 'ClientData.json', true);
+    dataRequest.send();
 }
 
-function searchFullName(fullname) {
-	
-	//var r=JSON.parse(xhr.responseText);
-	document.getElementById("searchvalue").innerHTML="Search by Full Name"+"<br>";
-	//structure table
-	var output="<tr><th>Full Name</th><th>Address</th><th>City </th><th>ID Number</th><th>Company</th></tr>";
-	var searchname;
-	for(var i=0; i<r.length; i++)
-	{
-		var obj=r[i];
-		searchname=obj.name;
-		if(searchname.startsWith(fullname))
-		{	
-				
-					output+="<tr><td>"
-					output+=obj.name;
-					output+="</td><td>"
-					output+=obj.address
-					output+="</td><td>"
-					output+=obj.city
-					output+="</td><td>"
-					output+=obj.idnumber
-					output+="</td><td>"
-					output+=obj.company
-					output+="</td></tr>";
-			}
-				
+function searchName(name) {
+	document.getElementById('searchCriteria').textContent = searchBy + 'Name';
+    document.getElementById('searchResults').innerHTML = searchHeadings;
+	let client;
+    let results;
+	for (let i = 0; i < clientData.length; i++) {
+		client = clientData[i];
+		if (client.lastName.startsWith(name)) {
+            results += '<tr><td>';
+            results += client.id;
+            results += '</td><td>';
+            results += client.firstName;
+            results += '</td><td>';
+            results += client.lastName;
+            results += '</td><td>';
+            results += client.address;
+            results += '</td><td>';
+            results += client.postalCode;
+            results += '</td><td>';
+            results += client.phone;
+            results += '</td><td>';
+            results += client.type;
+            results += '</td></tr>';
+		}			
 	}
-document.getElementById("searchresults").innerHTML=output;
+    document.getElementById('searchResults').innerHTML += results;
 
 
 }
 function searchIDNumber(idnumber) {
 
-	//var r=JSON.parse(xhr.responseText);
-	document.getElementById("searchvalue").innerHTML="Search by ID Number"+"<br>";
+	//var r=JSON.parse(dataRequest.responseText);
+	document.getElementById('searchvalue').innerHTML='Search by ID Number'+'<br>';
 	//structure table
-	var output="<tr><th>Full Name</th><th>Address</th><th>City </th><th>ID Number</th><th>Company</th></tr>";
+	var results='<tr><th>Full Name</th><th>Address</th><th>City </th><th>ID Number</th><th>Company</th></tr>';
 	var searchid;
 	for(var i=0; i<r.length; i++)
 	{
-		var obj=r[i];
-		searchid=obj.idnumber;
+		var client=r[i];
+		searchid=client.idnumber;
 		if(searchid.startsWith(idnumber))
 		{
 
-			output+="<tr><td>"
-			output+=obj.name;
-			output+="</td><td>"
-			output+=obj.address
-			output+="</td><td>"
-			output+=obj.city
-			output+="</td><td>"
-			output+=obj.idnumber
-			output+="</td><td>"
-			output+=obj.company
-			output+="</td></tr>";
+			results+='<tr><td>'
+			results+=client.name;
+			results+='</td><td>'
+			results+=client.address
+			results+='</td><td>'
+			results+=client.city
+			results+='</td><td>'
+			results+=client.idnumber
+			results+='</td><td>'
+			results+=client.company
+			results+='</td></tr>';
 		}
 
 	}
-	document.getElementById("searchresults").innerHTML=output;
+	document.getElementById('searchresults').innerHTML=results;
 
 
 }
 
 function searchCompanyName(company) {
 
-	//var r=JSON.parse(xhr.responseText);
-	document.getElementById("searchvalue").innerHTML="Search by Company name"+"<br>";
+	//var r=JSON.parse(dataRequest.responseText);
+	document.getElementById('searchvalue').innerHTML='Search by Company name'+'<br>';
 	//structure table
-	var output="<tr><th>Full Name</th><th>Address</th><th>City </th><th>ID Number</th><th>Company</th></tr>";
+	var results='<tr><th>Full Name</th><th>Address</th><th>City </th><th>ID Number</th><th>Company</th></tr>';
 	var searchcompany;
 	for(var i=0; i<r.length; i++)
 	{
-		var obj=r[i];
-		searchcompany=obj.company;
+		var client=r[i];
+		searchcompany=client.company;
 		if(searchcompany.startsWith(company))
 		{
 
-			output+="<tr><td>"
-			output+=obj.name;
-			output+="</td><td>"
-			output+=obj.address
-			output+="</td><td>"
-			output+=obj.city
-			output+="</td><td>"
-			output+=obj.idnumber
-			output+="</td><td>"
-			output+=obj.company
-			output+="</td></tr>";
+			results+='<tr><td>'
+			results+=client.name;
+			results+='</td><td>'
+			results+=client.address
+			results+='</td><td>'
+			results+=client.city
+			results+='</td><td>'
+			results+=client.idnumber
+			results+='</td><td>'
+			results+=client.company
+			results+='</td></tr>';
 		}
 
 	}
-	document.getElementById("searchresults").innerHTML=output;
+	document.getElementById('searchresults').innerHTML=results;
 
 
 }
+
+setEventListeners();
+document.getElementById('searchResults').innerHTML = searchHeadings;
